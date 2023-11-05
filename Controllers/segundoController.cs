c5e2bb6ae74051d73291bfbf84ceda3a08846ed1
Jhon Nova <jhonnovax@gmail.com>
2023-11-05 07:03:12 -0500

    segundo commit

---
+++ Controllers/segundoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class segundoController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+       {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public segundoController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "Get1")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
